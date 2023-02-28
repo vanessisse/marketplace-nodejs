676b4136cb3e62f662dde6b02564bd3fbec1cfbe
vanessisse <vanessasillos@gmail.com>
2023-02-27 23:58:11 -0300

    Feat: Scripts, rotas de usuário e controller

---
+++ src/router/usuario.router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const usuarioController = require('../controller/usuario.controller')
+
+router.get('/findById:id');
+router.get('/findAll');
+
+router.post('/create');
+router.post('/addAdress/:id');
+router.post('/addFavProduct/:id');
+
+router.put('/update/:id');
+
+router.delete('/remove/:id');
+router.delete('/removeAdress');
+router.delete('/removeFavProduct');
+
+module.exports = router;
+
