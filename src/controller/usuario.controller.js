917ec74ee04b9f63cb1a231f33279a8eeacdb1ee
vanessisse <vanessasillos@gmail.com>
2023-03-01 20:29:28 -0300

    Fix: validações em addAdress e removeAdress

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -82,10 +82,12 @@ const addUserAddressController = async (req, res) => {
         req.body.createdAt = new Date();
         const endereco = await userService.addUserAddressService(req.params.id, req.body);
 
-        if(endereco.ok == 1){
-            res.status(201).send({ message: 'Endereço adicionado com sucesso!' });
-        }else{
+        console.log(endereco);
+        
+        if(endereco.value == null){
             res.status(400).send({ message: 'Algo deu errado no endereço. Tente novamente.' });
+        }else{
+            res.status(201).send({ message: 'Endereço adicionado com sucesso!' });
         }
 
     } catch (err) {
@@ -97,8 +99,15 @@ const addUserAddressController = async (req, res) => {
 const removeUserAddressControler = async (req, res) => {
     try {
         const endereco = await userService.removeUserAddressService(req.body.id, req.body.addressId);
+        let found = false;
 
-        if(endereco.ok == 1){
+        endereco.value.enderecos.map((valor, chave) => {
+            if(valor._id == req.body.addressId){
+                found = true;
+            }
+        })
+
+        if(found){
             res.status(200).send({ message: 'Endereço removido com sucesso!' });
         }else{
             res.status(400).send({ message: 'Algo deu errado. Endereço não removido. Tente novamente.' });
