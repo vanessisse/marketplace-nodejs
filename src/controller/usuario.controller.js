8fd801058e85705e20b4310c76c6c4181841e0c0
vanessisse <vanessasillos@gmail.com>
2023-03-01 00:05:03 -0300

    Feat: user controller atualizado

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -1,7 +1,21 @@
+const userService = require('../service/usuario.service');
+
 const findUserByIdController = async (req, res) => {
     try{
+        const user = await userService.findUserByIdService(req.params.id);
+
+        if(!user){
+            return res.status(404).send({message: 'Usuário não encontrado. Tente novamente.'});
+        }
+
+        return res.status(200).send(user);
 
     }catch (err){
+        if(err.kind == 'ObjectID'){
+            console.log(err.kind == 'ObjectId');
+            return res.status(400).send({message: 'O ID informado está incorreto. Tente novamente.'});
+        }
+
         console.log(`Erro: ${err.message}`)
         return res.status(500).send({ message: 'Erro! Tente novamente.'});
     }
@@ -9,7 +23,7 @@ const findUserByIdController = async (req, res) => {
 
 const findAllUsersCrontoller = async (req, res) => {
     try{
-
+        return res.status(200).send(await userService.findAllUsersService());        
     }catch (err){
         console.log(`Erro: ${err.message}`)
         return res.status(500).send({ message: 'Erro! Tente novamente.'});
@@ -18,6 +32,12 @@ const findAllUsersCrontoller = async (req, res) => {
 
 const createUserController = async (req, res) => {
     try{
+        const body = req.body;
+        if(!body.nome){
+            return res.status(400).send({message: 'O campo "nome" precisa ser preenchido.'});
+        }
+
+        return res.status(200).send(await userService.createUserService(body));
 
     }catch (err){
         console.log(`Erro: ${err.message}`)
@@ -27,6 +47,12 @@ const createUserController = async (req, res) => {
 
 const updateUserController =  async (req, res) => {
     try{
+        const body = req.body;
+        if(!body.nome){
+            return res.status(400).send({message: 'O campo "nome" precisa ser preenchido.'});
+        }
+
+        return res.send(await userService.updateUserService(req.params.id, body));
 
     }catch (err){
         console.log(`Erro: ${err.message}`)
@@ -36,6 +62,13 @@ const updateUserController =  async (req, res) => {
 
 const removeUserController = async (req, res) => {
     try{
+        const deleteUser = await userService.removeUserService(req.params.id);
+
+        if(deleteUser.deletedCount > 0){
+            res.status(200).send({ message: 'Sucesso! Usuário excluído.'});
+        }else{
+            res.status(404).send({ message: 'Usuário não encontrado. Tente novamente.'});
+        }
 
     }catch (err){
         console.log(`Erro: ${err.message}`)
