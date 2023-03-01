39d30ae32bf9da870b007f7052852cb3e188558a
vanessisse <vanessasillos@gmail.com>
2023-03-01 00:42:32 -0300

    Fix: endereço do findById

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usuarioController = require('../controller/usuario.controller')
 
 // Rotas GET
-router.get('/findById:id', usuarioController.findUserByIdController);
+router.get('/findById/:id', usuarioController.findUserByIdController);
 router.get('/findAll', usuarioController.findAllUsersCrontoller);
 
 // Rotas POST
