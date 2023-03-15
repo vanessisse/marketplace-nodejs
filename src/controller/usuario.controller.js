0971d45e526c92adddf2b6915e5c264d6682b665
vanessisse <vanessasillos@gmail.com>
2023-03-15 16:53:57 -0300

    Fix: status da requisição de remoção de produto

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -130,7 +130,7 @@ const addUserFavProductController = async (req, res) => {
 
 const removeUserFavProductController = async (req, res) => {
     try {
-        res.status(201).send(await userService.removeUserFavProductService(req.params.id, req.body));
+        res.status(200).send(await userService.removeUserFavProductService(req.params.id, req.body));
     } catch (err) {
         console.log(`Erro: ${err.message}`)
         return res.status(500).send({ message: 'Erro! Tente novamente.' });
